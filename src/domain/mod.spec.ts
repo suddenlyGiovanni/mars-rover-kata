d539ea8bf3cb6e82286c1475f4208a24174a7502
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-03-16 22:47:00 +0100

    Add tests for rover movement commands
    Extended test coverage by adding scenarios for rover's movement commands (forward) in all orientations: North, South, East, and West. These tests ensure correct position updates and handling of grid boundaries. Placeholder added for backward movement tests.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- src/domain/mod.spec.ts
+++ src/domain/mod.spec.ts
@@ -27,8 +27,8 @@ import { Rover } from './rover.ts'
 describe('move', () => {
 	describe('responds to the orientation changes commands', () => {
 		const size = new GridSize({
-			width: GridSize.Width(4),
-			height: GridSize.Height(3),
+			width: GridSize.Width(5),
+			height: GridSize.Height(4),
 		})
 		const planet = new Planet({ size })
 
@@ -104,6 +104,321 @@ describe('move', () => {
 					expect(actualOrientation).toEqual(expectedNextOrientation)
 				},
 			)
+		})
+	})
+	describe('responds to the movement commands', () => {
+		const size = new GridSize({
+			width: GridSize.Width(5),
+			height: GridSize.Height(4),
+		})
+		const planet = new Planet({ size })
+
+		const initialPosition = new Position({ x: Position.X(0), y: Position.Y(0) })
+		const initialOrientation = Orientation.North()
+
+		const rover = new Rover({
+			position: initialPosition,
+			orientation: initialOrientation,
+		})
+
+		/**
+		 * Given a world of with 4 and height 3
+		 * and a Rover located at position `[x: 0, y:0]` oriented North
+		 * as depicted by the following ascii art map
+		 *
+		 *                   North
+		 *         +-----+-----+-----+-----+-----+
+		 *         | 0,3 |     |     |     | 4,3 |
+		 *         +-----+-----+-----+-----+-----+
+		 *         |     |     |     |     |     |
+		 *   West  +-----+-----+-----+-----+-----+  Est
+		 *         |     |     |     |     |     |
+		 *         +-----+-----+-----+-----+-----+
+		 *         |[0,0]|     |     |     | 4,0 |
+		 *         +-----+-----+-----+-----+-----+
+		 *                   South
+		 */
+		describe('responds to the move commands', () => {
+			describe('Should go forward', () => {
+				describe('Given a North orientation', () => {
+					const rover = new Rover({
+						position: initialPosition,
+						orientation: Orientation.North(),
+					})
+
+					it.each([
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(1) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(1) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(2) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(2) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(3) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(3) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+						},
+					])(
+						'Given the initial position `[$initial.position.x,$initial.position.y]`, it should move to `[$expected.position.x,$expected.position.y]`',
+						({ initial, expected }) => {
+							const { position: actualPosition } = move(
+								initial,
+								planet,
+								Command.GoForward(),
+							)
+
+							/**
+							 *                   North
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,3]|     |     |     | 4,3 |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,2]|     |     |     |     |
+							 *   West  +-----+-----+-----+-----+-----+  Est
+							 *         |[0,1]|     |     |     |     |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,0]|     |     |     | 4,0 |
+							 *         +-----+-----+-----+-----+-----+
+							 *                   South
+							 */
+							expect(actualPosition).toEqual(expected.position)
+						},
+					)
+				})
+
+				describe('Given a South orientation', () => {
+					const rover = new Rover({
+						position: initialPosition,
+						orientation: Orientation.South(),
+					})
+					it.each([
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(3) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(3) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(2) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(2) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(1) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(1) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+						},
+					])(
+						'Given the initial position `[$initial.position.x,$initial.position.y]`, it should move to `[$expected.position.x,$expected.position.y]`',
+						({ initial, expected }) => {
+							const { position: actualPosition } = move(
+								initial,
+								planet,
+								Command.GoForward(),
+							)
+
+							/**
+							 *                   North
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,3]|     |     |     | 4,3 |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,2]|     |     |     |     |
+							 *   West  +-----+-----+-----+-----+-----+  Est
+							 *         |[0,1]|     |     |     |     |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,0]|     |     |     | 4,0 |
+							 *         +-----+-----+-----+-----+-----+
+							 *                   South
+							 */
+							expect(actualPosition).toEqual(expected.position)
+						},
+					)
+				})
+
+				describe('Given a Est orientation', () => {
+					const rover = new Rover({
+						position: initialPosition,
+						orientation: Orientation.Est(),
+					})
+					it.each([
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(1), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(1), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(2), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(2), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(3), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(3), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(4), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(4), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+						},
+					])(
+						'Given the initial position `[$initial.position.x,$initial.position.y]`, it should move to `[$expected.position.x,$expected.position.y]`',
+						({ initial, expected }) => {
+							const { position: actualPosition } = move(
+								initial,
+								planet,
+								Command.GoForward(),
+							)
+
+							/**
+							 *                   North
+							 *         +-----+-----+-----+-----+-----+
+							 *         |     |     |     |     | 4,3 |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |     |     |     |     |     |
+							 *   West  +-----+-----+-----+-----+-----+  Est
+							 *         |     |     |     |     |     |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,0]|[1,0]|[2,0]|[3,0]|[4,0]|
+							 *         +-----+-----+-----+-----+-----+
+							 *                   South
+							 */
+							expect(actualPosition).toEqual(expected.position)
+						},
+					)
+				})
+
+				describe('Given a West orientation', () => {
+					const rover = new Rover({
+						position: initialPosition,
+						orientation: Orientation.West(),
+					})
+					it.each([
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(4), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(4), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(3), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(3), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(2), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(2), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(1), y: Position.Y(0) }),
+							}),
+						},
+						{
+							initial: rover.clone({
+								position: new Position({ x: Position.X(1), y: Position.Y(0) }),
+							}),
+							expected: rover.clone({
+								position: new Position({ x: Position.X(0), y: Position.Y(0) }),
+							}),
+						},
+					])(
+						'Given the initial position `[$initial.position.x,$initial.position.y]`, it should move to `[$expected.position.x,$expected.position.y]`',
+						({ initial, expected }) => {
+							const { position: actualPosition } = move(
+								initial,
+								planet,
+								Command.GoForward(),
+							)
+
+							/**
+							 *                   North
+							 *         +-----+-----+-----+-----+-----+
+							 *         |     |     |     |     | 4,3 |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |     |     |     |     |     |
+							 *   West  +-----+-----+-----+-----+-----+  Est
+							 *         |     |     |     |     |     |
+							 *         +-----+-----+-----+-----+-----+
+							 *         |[0,0]|[1,0]|[2,0]|[3,0]|[4,0]|
+							 *         +-----+-----+-----+-----+-----+
+							 *                   South
+							 */
+							expect(actualPosition).toEqual(expected.position)
+						},
+					)
+				})
+			})
+
+			describe.todo('Should go backward')
 		})
 	})
 })
