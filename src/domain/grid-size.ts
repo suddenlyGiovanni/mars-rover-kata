57fca7981d9b9ea3cbd291ca80ce3c5b3451a360
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-03-16 01:35:11 +0100

    Add GridSize class with width and height properties
    Introduce a `GridSize` class to represent grid dimensions with immutable width and height properties. Includes `Width` and `Height` branded types for structured validation and construction. This provides a clear, type-safe way to handle grid sizing.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ src/domain/grid-size.ts
@@ -0,0 +1,58 @@
+import { Data, Schema } from 'effect'
+
+import { Int } from '../types.ts'
+
+const Width = Int.pipe(Schema.brand('Width'))
+export declare namespace Width {
+	export type Type = Schema.Schema.Type<typeof Width>
+}
+
+const Height = Int.pipe(Schema.brand('Height'))
+export declare namespace Height {
+	export type Type = Schema.Schema.Type<typeof Height>
+}
+
+/**
+ * Represents the dimensions of a grid with fixed width and height properties.
+ *
+ * This class is a tagged class extension providing a structured way to define
+ * and work with grid dimensions. Instances of GridSize are immutable, with
+ * `width` and `height` being read-only.
+ *
+ * The width represents the number of horizontal units in the grid.
+ * The height represents the number of vertical units in the grid.
+ *
+ * Example 5x4 grid with coordinates:
+ *
+ *                     North
+ *           +-----+-----+-----+-----+-----+
+ *           | 0,3 | 1,3 | 2,3 | 3,3 | 4,3 |
+ *           +-----+-----+-----+-----+-----+
+ *           | 0,2 | 1,2 | 2,2 | 3,2 | 4,2 |
+ *     West  +-----+-----+-----+-----+-----+  East
+ *           | 0,1 | 1,1 | 2,1 | 3,1 | 4,1 |
+ *           +-----+-----+-----+-----+-----+
+ *           | 0,0 | 1,0 | 2,0 | 3,0 | 4,0 |
+ *           +-----+-----+-----+-----+-----+
+ *                     South
+ */
+export class GridSize extends Data.TaggedClass('GridSize')<{
+	readonly width: Width.Type
+	readonly height: Height.Type
+}> {
+	/**
+	 * Width constructor
+	 */
+	public static readonly Width: (
+		int: number,
+		options?: Schema.MakeOptions,
+	) => Width.Type = Width.make
+
+	/**
+	 * Height constructor
+	 */
+	public static readonly Height: (
+		int: number,
+		options?: Schema.MakeOptions,
+	) => Height.Type = Height.make
+}
