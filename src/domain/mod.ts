bf57f91dfedd10768cd496e607715d314caff6d3
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-03-16 22:47:33 +0100

    Implement non-negative modulus and partial forward movement
    Added a utility function `nonNegativeModulus` to handle safe modulus operations. Updated the `GoForward` command to handle movement for the `North` orientation with wrapping logic, while stubs remain for other orientations. Left placeholders (`hole()`) for incomplete commands and movement logic to be implemented later.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- src/domain/mod.ts
+++ src/domain/mod.ts
@@ -1,9 +1,22 @@
-import { pipe } from 'effect'
+import { hole, pipe } from 'effect'
 
+import { Int } from '../types.ts'
 import { Command } from './command.ts'
 import { Orientation } from './orientation.ts'
 import type { Planet } from './planet.ts'
+import { Position } from './position.ts'
 import type { Rover } from './rover.ts'
+
+/**
+ * Calculates the modulus of two integers, ensuring the result is always non-negative.
+ *
+ * @param dividend - The number to be divided.
+ * @param divisor - The number by which the dividend is divided.
+ * @return The non-negative remainder of the division operation.
+ */
+function nonNegativeModulus(dividend: Int.Type, divisor: Int.Type): Int.Type {
+	return Int.make(Math.abs(dividend % divisor))
+}
 
 /**
  *  Implement wrapping from one edge of the grid to another (pacman effect).
@@ -34,8 +47,27 @@ export function move(rover: Rover, planet: Planet, command: Command): Rover {
 						West: () => rover.clone({ orientation: Orientation.North() }),
 					}),
 				),
-			GoForward: () => rover,
-			GoBackward: () => rover,
+			GoForward: () =>
+				pipe(
+					rover.orientation,
+					Orientation.$match({
+						North: () =>
+							rover.clone({
+								position: rover.position.clone({
+									y: Position.Y(
+										nonNegativeModulus(
+											Int.make(rover.position.y + 1),
+											planet.size.height,
+										),
+									),
+								}),
+							}),
+						South: () => hole(),
+						West: () => hole(),
+						Est: () => hole(),
+					}),
+				),
+			GoBackward: () => hole(),
 		}),
 	)
 }
