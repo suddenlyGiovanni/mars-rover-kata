268c446ab041b226f316e2ff57fcd887fb7fdd0f
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-03-16 02:55:48 +0100

    Add Deno config, tasks, and Vite plugin integration
    This commit updates the Deno configuration by adding schema, tasks, and lint rules. It includes the Vite plugin for better compatibility and updates the `.gitignore` and lock files accordingly. Test configuration has been refined to improve source inclusion.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vitest/config'
+import deno from "@deno/vite-plugin"
 
 export default defineConfig({
+	plugins: [deno()],
 	test: {
-		includeSource: ['src/**/*.{js,ts}'],
+		includeSource: ['src/**/*.ts'],
+		include: ['src/**/*.spec.ts'],
 	},
 })
